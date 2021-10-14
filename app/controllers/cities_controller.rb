7af21f966f5f9eaa774eed25cc8cbbb7a7968b35
Ryochi <ryo1.t.zubu@gmail.com>
2021-10-15 09:51:15 +1000

    Done

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,11 @@
 class CitiesController < ApplicationController
+
+    def index
+        @cities = City.ApplicationController
+    end
+
+    def show
+        @city = City.find(params[:id])
+        @listings = @city.listings
+    end
 end
